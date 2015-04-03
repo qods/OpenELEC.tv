50c0e461480b3630323bb4721d89f27c12332bee
Stefan Saraev <stefan@saraev.ca>
2015-04-03 09:20:32 +0200

    kodi-audioencoder-flac: move to package 'kodi-audioencoder.flac'
    Signed-off-by: Stephan Raue <stephan@openelec.tv>

--- packages/mediacenter/kodi-audioencoder.flac/package.mk
+++ packages/mediacenter/kodi-audioencoder.flac/package.mk
@@ -16,21 +16,23 @@
 #  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
-PKG_NAME="kodi-audioencoder-flac"
-PKG_VERSION="62c2cc8"
+PKG_NAME="kodi-audioencoder.flac"
+PKG_VERSION="84acb14"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
-PKG_URL="$DISTRO_SRC/$PKG_NAME-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain flac kodi"
+PKG_URL="http://saraev.ca/openelec/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform flac"
 PKG_PRIORITY="optional"
-PKG_SECTION="multimedia"
-PKG_SHORTDESC="kodi-audioencoder-flac: A audioencoder addon for Kodi"
-PKG_LONGDESC="kodi-audioencoder-flac is a audioencoder addon for Kodi"
-
-PKG_IS_ADDON="no"
+PKG_SECTION=""
+PKG_SHORTDESC="audioencoder.flac: A audioencoder addon for Kodi"
+PKG_LONGDESC="audioencoder.flac is a audioencoder addon for Kodi"
 PKG_AUTORECONF="no"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_ID="audioencoder.flac"
+PKG_ADDON_TYPE="xbmc.audioencoder"
 
 configure_target() {
   cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
@@ -40,4 +42,10 @@ configure_target() {
         -DFLAC_INCLUDE_DIRS=$SYSROOT_PREFIX/usr/include \
         -DOGG_INCLUDE_DIRS=$SYSROOT_PREFIX/usr/include \
         ..
+}
+
+addon() {
+  mkdir -p $ADDON_BUILD/$PKG_ADDON_ID/
+  cp -PR $PKG_BUILD/.install_pkg/usr/share/kodi/addons/$PKG_ADDON_ID/* $ADDON_BUILD/$PKG_ADDON_ID/
+  cp -PL $PKG_BUILD/.install_pkg/usr/lib/kodi/addons/$PKG_ADDON_ID/*.so $ADDON_BUILD/$PKG_ADDON_ID/
 }
